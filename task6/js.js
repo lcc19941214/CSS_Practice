0b2722a8d74d8bbef13e9e0b141ee67bf99143a4
潘达桑Conan <245987239@qq.com>
2015-12-01 01:16:19 +0800

    update task6

--- task6/js.js
+++ task6/js.js
@@ -1,1 +1,22 @@
-	
+var music = document.getElementById("u-music");
+
+function showTool(){
+	var mPlayers = document.getElementsByClassName("m-players")[0];
+	var uPlayer = mPlayers.getElementsByClassName("u-player");
+	var toolList = mPlayers.getElementsByClassName("u-player--tools");
+	for (var i = 0; i < uPlayer.length - 1; i++) {
+		uPlayer[i].index = i;
+		uPlayer[i].onmouseover = function(){
+			toolList[this.index].style.display = "block";
+		}
+		uPlayer[i].onmouseout = function(){
+			for (var i = 0; i < toolList.length; i++) {
+				toolList[i].style.display = "none";
+			};
+		}
+	}
+}
+
+window.onload = function(){
+	showTool();
+}
